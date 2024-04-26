1e94020056ab9222a5d9ceba05aa4e002c0b0c59
Abhishek kumar <guptabhi49@gmail.com>
2024-04-26 19:09:16 +0530

    Modified validate.js

--- src/utils/Validate.js
+++ src/utils/Validate.js
@@ -2,7 +2,7 @@ export const checkValidateData = (email,password,fullName,isSignUp)=>{
     const isEmailValid = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)
     const isPasswordvalid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password)
     const isFullNamevalid = isSignUp? fullName.trim().length>0:true;
-    if(!isFullNamevalid) return "Please enter ful test name"
+    if(!isFullNamevalid) return "Please enter Full Name"
     if(!isEmailValid) return "Email is not valid"
     if(!isPasswordvalid) return "Password is not valid"
     
